07010311b321eb510be0a3866343d0fc3b1001ca
Victor Bernardino <victorbernardinofc@gmail.com>
2026-09-14 20:46:59 -0300

    Adiciona estilos e evento de clique para o menu

---
+++ java.js/javascript.js
@@ -0,0 +1,6 @@
+const botaoMenu = document.querySelector('.menu-toggle');
+const menuPrincipal = document.getElementById('menu-principal');
+
+botaoMenu.addEventListener('click',function () {
+menu.classlist.toggle("ativo");
+});
