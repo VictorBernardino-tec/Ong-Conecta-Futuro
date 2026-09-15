a197c3c7d012a68d58bddd64c058a859277adc79
Victor Bernardino <victorbernardinofc@gmail.com>
2026-09-14 21:04:52 -0300

    Ajusta visibilidade do menu e suaviza sombra dos cards

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 const botaoMenu = document.querySelector(".menu-toggle");
 const menu = document.querySelector("#menu-principal");
-
 botaoMenu.addEventListener("click", function () {
     menu.classList.toggle("ativo");
-
     const menuAberto = menu.classList.contains("ativo");
-
     botaoMenu.setAttribute("aria-expanded", menuAberto);
 });
